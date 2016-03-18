f87cbe7212b4c418f78641c423c4bc5844e84caf
epaull <evanpaull@gmail.com>
2016-03-17 22:11:18 -0700

    --fix file locations

--- scripts/VIPER/viper.sh
+++ scripts/VIPER/viper.sh
@@ -14,21 +14,21 @@ echo $sample_data
 echo $sample_name
 # add data and regulon to /tmp
 mkdir -p tmp
-$BASEDIR/../PERL-TOOLS/perl/join.pl $sample_data $normal_data > tmp/data.tab
-cp $regulon tmp
+$BASEDIR/../PERL-TOOLS/perl/join.pl $sample_data $normal_data > `pwd`/tmp/data.tab
+cp $regulon `pwd`/tmp
 
 # docker run 
 mkdir -p output
 docker run --rm \
-	-v output:/output \
-	-v tmp:/data \
+	-v "`pwd`/output":/output \
+	-v "`pwd`/tmp":/data \
 	ucsc-viper-unsupervised \
 	-e /data/data.tab \
 	-n /data/$regulon \
 	--output /output
 
-result=/tmp/viperScores.txt
+result="`pwd`"/output/viperScores.txt
 head -n 1 $result > /tmp/sample.txt
-grep '^$sample_name' $result >> /tmp/sample.txt
+grep "^$sample_name" $result >> /tmp/sample.txt
 $BASEDIR/../PERL-TOOLS/perl/transpose.pl /tmp/sample.txt > /tmp/vpInferences.${sample_name}.tab
 
